71cf3d2e8d5375079bd5499362011096c9ea328e
Togglebit <me@togglebit.io>
2021-07-19 12:33:13 +0200

    getting lunch

--- src/connections.rs
+++ src/connections.rs
@@ -10,24 +10,24 @@ pub fn handle_client(
 ) {
     let mut con_locked = connections.lock().expect("Failed to lock connections");
 
+    // Add sender to connection list
     let (sender, receiver) = channel();
     con_locked.push(sender);
     drop(con_locked);
 
 
+    // Clone the tcp stream so we can have both a reader thread and a writer thread
     let writer = reader.try_clone().expect("Failed to clone the connection");
 
-    thread::spawn(move || {
-        read(reader);
-    });
-
-    thread::spawn(move || {
-        write(writer);
-    });
+    thread::spawn(move || { read(reader); });
+    thread::spawn(move || { write(writer, connections); });
 }
 
 fn read(stream: TcpStream) {
 }
 
-fn write(stream: TcpStream) {
+fn write(
+    stream: TcpStream,
+    connections: Arc<Mutex<Vec<Sender<String>>>>
+) {
 }
